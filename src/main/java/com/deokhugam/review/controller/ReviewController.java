da364d78d02b1b2b9579ecca2183bb1aa2693b56
mango <dhfqor1101@gmail.com>
2026-08-19 15:06:36 +0900

    feat: 리뷰 등록 API 컨트롤러 구현

--- src/main/java/com/deokhugam/review/controller/ReviewController.java
+++ src/main/java/com/deokhugam/review/controller/ReviewController.java
@@ -1,4 +1,32 @@
 package com.deokhugam.review.controller;
 
+import com.deokhugam.review.dto.request.ReviewCreateRequest;
+import com.deokhugam.review.dto.response.ReviewDetailResponse;
+import com.deokhugam.review.service.ReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reviews")
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<ReviewDetailResponse> create(
+            @Valid @RequestBody ReviewCreateRequest request
+    ) {
+        ReviewDetailResponse response = reviewService.create(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
 }
