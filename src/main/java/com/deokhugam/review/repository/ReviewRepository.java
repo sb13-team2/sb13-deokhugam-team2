da7a22f4ec2fd4db46820acc313711c7bf5e9463
mango <dhfqor1101@gmail.com>
2026-08-19 11:35:55 +0900

    feat: 활성 리뷰 중복 조회 Repository 구현

--- src/main/java/com/deokhugam/review/repository/ReviewRepository.java
+++ src/main/java/com/deokhugam/review/repository/ReviewRepository.java
@@ -1,4 +1,13 @@
 package com.deokhugam.review.repository;
 
-public interface ReviewRepository {
+import com.deokhugam.review.entity.Review;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, UUID> {
+
+    boolean existsByUserIdAndBookIdAndDeletedAtIsNull(
+            UUID userId,
+            UUID bookId
+    );
 }
