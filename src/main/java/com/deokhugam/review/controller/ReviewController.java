bfa73d3aa2f4753c5c5e8013889d3f9a2079321e
mango <dhfqor1101@gmail.com>
2026-08-20 16:38:21 +0900

    feat: 리뷰 상세 조회 API 구현

--- src/main/java/com/deokhugam/review/controller/ReviewController.java
+++ src/main/java/com/deokhugam/review/controller/ReviewController.java
@@ -9,13 +9,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/reviews")
@@ -36,6 +30,19 @@ public class ReviewController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @GetMapping("/{reviewId}")
+    public ResponseEntity<ReviewDetailResponse> findById(
+            @PathVariable UUID reviewId,
+            @RequestHeader(REQUEST_USER_ID_HEADER) UUID requesterId
+    ) {
+        ReviewDetailResponse response = reviewService.findById(
+                reviewId,
+                requesterId
+        );
+
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/{reviewId}")
