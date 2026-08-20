25e0c36863ab58f0aa8bd4ee183288002a85bc81
mango <dhfqor1101@gmail.com>
2026-08-20 16:15:12 +0900

    feat: 리뷰 좋아요 엔티티 구현

--- src/main/java/com/deokhugam/review/entity/ReviewLike.java
+++ src/main/java/com/deokhugam/review/entity/ReviewLike.java
@@ -1,4 +1,76 @@
 package com.deokhugam.review.entity;
 
+import com.deokhugam.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(
+        name = "review_likes",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_review_likes_review_user",
+                        columnNames = {"review_id", "user_id"}
+                )
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class ReviewLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "UUID")
+    private UUID id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreatedDate
+    @Column(
+            name = "created_at",
+            nullable = false,
+            updatable = false
+    )
+    private LocalDateTime createdAt;
+
+    private ReviewLike(
+            Review review,
+            User user
+    ) {
+        this.review = review;
+        this.user = user;
+    }
+
+    public static ReviewLike create(
+            Review review,
+            User user
+    ) {
+        return new ReviewLike(review, user);
+    }
 }
