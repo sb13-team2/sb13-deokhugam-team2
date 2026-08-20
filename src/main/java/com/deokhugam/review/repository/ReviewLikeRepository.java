b13923cd84af8b19c4c504dca42232867f2c02c0
mango <dhfqor1101@gmail.com>
2026-08-20 16:18:53 +0900

    feat: 리뷰 좋아요 여부 조회 Repository 구현

--- src/main/java/com/deokhugam/review/repository/ReviewLikeRepository.java
+++ src/main/java/com/deokhugam/review/repository/ReviewLikeRepository.java
@@ -1,4 +1,14 @@
 package com.deokhugam.review.repository;
 
-public interface ReviewLikeRepository {
+import com.deokhugam.review.entity.ReviewLike;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewLikeRepository
+        extends JpaRepository<ReviewLike, UUID> {
+
+    boolean existsByReviewIdAndUserId(
+            UUID reviewId,
+            UUID userId
+    );
 }
